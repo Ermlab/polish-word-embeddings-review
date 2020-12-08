a702024680c2844d40d3c56917d957ee94bcd9d2
Juliusz Lewalski <juliusz.lewalski@gmail.com>
2020-12-08 11:02:50 +0100

    refactor(evaluation): add loading .bin models and fasttext evaluation
    blacked

--- src/evaluation/__main__.py
+++ src/evaluation/__main__.py
@@ -6,39 +6,39 @@ from typing import Dict, Generator
 
 from evaluation.word_analogy import WordAnalogyW2VFormat
 
-WORD_ANALOGY_CSV_PATH = os.path.abspath('../data/analogy.csv')
-WORD_PAIRS_CSV_PATH = os.path.abspath('../data/word_pairs.csv')
+WORD_ANALOGY_CSV_PATH = os.path.abspath("../data/analogy.csv")
+WORD_PAIRS_CSV_PATH = os.path.abspath("../data/word_pairs.csv")
 
 
-def save_to_csv(dict_gen: Generator[Dict, None, None], path):
+def save_to_csv(data, path):
     file_exists = os.path.exists(path)
-    with open(path, 'a+') as csv_file:
-        data_to_save = list(dict_gen)
-        keys_from_dict = list(data_to_save[0].keys())
+    with open(path, "a+") as csv_file:
+        keys_from_dict = list(data[0].keys())
         csv_writer = csv.DictWriter(csv_file, keys_from_dict)
         if not file_exists:
             csv_writer.writeheader()
-        csv_writer.writerows(data_to_save)
+        csv_writer.writerows(data)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]),
-                                     formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description=__doc__)
-    parser.add_argument('-i',
-                        '--input',
-                        type=str,
-                        required=True,
-                        help='Directory or Path to word2vec format file.')
-    parser.add_argument('-a',
-                        '--analogy-path',
-                        type=str,
-                        required=True,
-                        help='Path to questions words file with analogy words.')
-    parser.add_argument('-sp',
-                        '--simlex-path',
-                        type=str,
-                        help='Path to SimLex File file with words pairs.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog=os.path.basename(sys.argv[0]),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=__doc__,
+    )
+    parser.add_argument(
+        "-i", "--input", type=str, required=True, help="Directory or Path to word2vec format file."
+    )
+    parser.add_argument(
+        "-a",
+        "--analogy-path",
+        type=str,
+        required=True,
+        help="Path to questions words file with analogy words.",
+    )
+    parser.add_argument(
+        "-sp", "--simlex-path", type=str, help="Path to SimLex File file with words pairs."
+    )
 
     args = parser.parse_args()
 
@@ -46,10 +46,15 @@ if __name__ == '__main__':
     analogy_path = os.path.abspath(args.analogy_path)
 
     w2v_val = WordAnalogyW2VFormat(input_path, analogy_path)
-    analogy_gen = w2v_val.evaluate_word_analogy()
-    save_to_csv(analogy_gen, WORD_ANALOGY_CSV_PATH)
+    analogy_list = []
+    words_pairs_list = []
+    for model, model_path in w2v_val.iter_models():
+        analogy_list.append(w2v_val.evaluate_word_analogy(model, model_path))
 
+        if args.simlex_path:
+            path_to_simlex = os.path.abspath(args.simlex_path)
+            words_pairs_list.append(w2v_val.evaluate_word_pairs(model, model_path, path_to_simlex))
+
+    save_to_csv(analogy_list, WORD_ANALOGY_CSV_PATH)
     if args.simlex_path:
-        path_to_simlex = os.path.abspath(args.simlex_path)
-        words_pairs = w2v_val.evaluate_word_pairs(path_to_simlex)
-        save_to_csv(words_pairs, WORD_PAIRS_CSV_PATH)
+        save_to_csv(words_pairs_list, WORD_PAIRS_CSV_PATH)
--- src/evaluation/word_analogy.py
+++ src/evaluation/word_analogy.py
@@ -3,6 +3,8 @@ import os
 
 import coloredlogs
 from gensim.models import KeyedVectors
+from gensim.models.fasttext import FastText
+
 
 class WordAnalogyW2VFormat:
     """
@@ -18,66 +20,89 @@ class WordAnalogyW2VFormat:
 
     def __init__(self, input_paths: str, path_to_analogy: str):
         if os.path.isdir(input_paths):
-            self.paths = [os.path.join(input_paths, path) for path in os.listdir(input_paths) if
-                          path.endswith('.txt')]
+            self.paths = [
+                os.path.join(input_paths, path)
+                for path in os.listdir(input_paths)
+                if path.endswith(".txt") or path.endswith(".bin")
+            ]
         else:
             self.paths = [os.path.abspath(input_paths)]
 
         self.path_to_analogy = os.path.abspath(path_to_analogy)
         self.logger = self.__init_logger()
 
-    def evaluate_word_analogy(self, more_info = False):
+    def iter_models(self):
+        for path in self.paths:
+            if path.endswith(".txt"):
+                model = KeyedVectors.load_word2vec_format(path)
+            elif path.endswith(".bin"):
+                model = KeyedVectors.load(path)
+            else:
+                self.logger.error("Wrong file extension. Should be .txt or .bin.")
+                continue
+            self.logger.info(f"\nPath: {path}")
+            if not isinstance(model, FastText):
+                self.logger.info(f"Vocab size = {len(model.wv.vocab)}")
+            self.logger.info(f"Vector size = {model.vector_size}")
+            yield model, path
+
+    def evaluate_word_analogy(self, model, model_path, more_info=False):
         """
         Evaluate every embedding in self.paths. Logs Path of embedding, Vocab and vector size, total and each section accuracy
         """
-        for path in self.paths:
-
-            model = KeyedVectors.load_word2vec_format(path)
+        if isinstance(model, FastText):
+            analogy = model.wv.evaluate_word_analogies(self.path_to_analogy)
+            len_vocab = len(model.wv.vocab)
+        else:
             analogy = model.evaluate_word_analogies(self.path_to_analogy)
+            len_vocab = len(model.vocab)
 
-            self.logger.info(f'\nPath: {path}')
-            self.logger.info(f'Vocab size = {len(model.vocab)}')
-            self.logger.info(f'Vector size = {model.vector_size}')
-            self.logger.info(f"Word analogies accuracy: {analogy[0]}\n")
-            if more_info:
-                for item in analogy[1]:
-                    sum_of_samples = (len(item['correct']) + len(item['incorrect']))
-                    if sum_of_samples == 0:
-                        continue
-                    acc = round((len(item['correct']) / sum_of_samples) * 100, 2)
+        self.logger.info(f"Word analogies accuracy: {analogy[0]}\n")
+        if more_info:
+            for item in analogy[1]:
+                sum_of_samples = len(item["correct"]) + len(item["incorrect"])
+                if sum_of_samples == 0:
+                    continue
+                acc = round((len(item["correct"]) / sum_of_samples) * 100, 2)
 
-                    self.logger.info(f"Section: {item['section']}")
-                    self.logger.info(f"Acc: {acc}")
-            yield {
-                'name': os.path.basename(path),
-                'vocab': len(model.vocab),
-                'vector': model.vector_size,
-                'analogy acc': analogy[0]
-            }
+                self.logger.info(f"Section: {item['section']}")
+                self.logger.info(f"Acc: {acc}")
 
-    def evaluate_word_pairs(self, path_to_simlex):
+        return {
+            "name": os.path.basename(model_path),
+            "vocab": len_vocab,
+            "vector": model.vector_size,
+            "analogy acc": analogy[0],
+        }
+
+    def evaluate_word_pairs(self, model, model_path, path_to_simlex):
         """
         Evaluate word pairs
         """
-        for path in self.paths:
-            model = KeyedVectors.load_word2vec_format(path)
-            similarity = model.evaluate_word_pairs(path_to_simlex)
-            pearson = similarity[0]
-            spearman = similarity[1]
-            self.logger.info(f'\nPath: {path}')
-            self.logger.info(f'Pearson correlation coefficient: {round(pearson[0], 4)}, p-value: {round(pearson[1], 4)}')
-            self.logger.info(f'Spearman correlation coefficient: {round(spearman.correlation, 4)}, p-value: {round(spearman.pvalue, 4)}')
-            self.logger.info(f'Out of vocab %: {round(similarity[2],4)}')
-            yield {
-                'name': os.path.basename(path),
-                'pearson': round(pearson[0], 4),
-                'spearman': round(spearman.correlation, 4),
-                'out of vocab': round(similarity[2], 4),
-            }
 
+        if isinstance(model, FastText):
+            similarity = model.wv.evaluate_word_pairs(path_to_simlex)
+        else:
+            similarity = model.evaluate_word_pairs(path_to_simlex)
+
+        pearson = similarity[0]
+        spearman = similarity[1]
+        self.logger.info(
+            f"Pearson correlation coefficient: {round(pearson[0], 4)}, p-value: {round(pearson[1], 4)}"
+        )
+        self.logger.info(
+            f"Spearman correlation coefficient: {round(spearman.correlation, 4)}, p-value: {round(spearman.pvalue, 4)}"
+        )
+        self.logger.info(f"Out of vocab %: {round(similarity[2], 4)}")
+        return {
+            "name": os.path.basename(model_path),
+            "pearson": round(pearson[0], 4),
+            "spearman": round(spearman.correlation, 4),
+            "out of vocab": round(similarity[2], 4),
+        }
 
     @staticmethod
-    def __init_logger(path_to_logger: str = '../data/w2v.log') -> logging.Logger:
+    def __init_logger(path_to_logger: str = "../data/w2v.log") -> logging.Logger:
         """
         Logger initializer
 
@@ -91,16 +116,16 @@ class WordAnalogyW2VFormat:
         ------
 
         """
-        formatter = logging.Formatter('%(message)s')
+        formatter = logging.Formatter("%(message)s")
         fh_logger_path = os.path.abspath(path_to_logger)
 
         log_level = os.getenv("LOG_LEVEL", "INFO")
-        log_level = 'CRITICAL' if log_level == 'false' else log_level
+        log_level = "CRITICAL" if log_level == "false" else log_level
 
         coloredlogs.install(level=log_level)
 
-        logger = logging.getLogger('WordEmbeddingLogger')
-        fh_logger = logging.FileHandler(filename=fh_logger_path, mode='a+')
+        logger = logging.getLogger("WordEmbeddingLogger")
+        fh_logger = logging.FileHandler(filename=fh_logger_path, mode="a+")
         fh_logger.setFormatter(formatter)
         logger.addHandler(fh_logger)
         return logger
